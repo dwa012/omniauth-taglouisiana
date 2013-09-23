58e5217ed583e3b0ba47bd14ff73a5d8dc837aa4
Daniel Ward <danielward@Daniels-MacBook-Pro.local>
2013-09-23 12:01:46 -0500

    fixed the auth hash and adjusted the site end point to be the test setup

--- lib/omniauth/strategies/taglouisiana.rb
+++ lib/omniauth/strategies/taglouisiana.rb
@@ -2,27 +2,32 @@ require 'omniauth/strategies/oauth2'
 
 module OmniAuth
   module Strategies
-    class TagLouisiana < OmniAuth::Strategies::OAuth2
+    class TagLouisiana < OAuth2
       # change the class name and the :name option to match your application name
       option :name, :taglouisiana
 
       option :client_options, {
-          :site => "https://moby.taglouisiana.com",
+          :site => "http://localhost:4000",
           :authorize_url => "/oauth/authorize"
       }
 
-      uid { raw_info["id"] }
+      uid do
+        raw_info["id"]
+      end
 
       info do
         {
             :email => raw_info["email"],
             :first_name => raw_info["first_name"],
-            :last_name => raw_info["last_name"]
+            :last_name => raw_info["last_name"],
+            :name => raw_info["name"]
         }
       end
 
       def raw_info
         @raw_info ||= access_token.get('/api/v1/me.json').parsed
+        puts "Raw info: " << @raw_info.to_json
+        @raw_info
       end
     end
   end
